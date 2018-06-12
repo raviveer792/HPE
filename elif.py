b6a46c3b81634c96bc95017482424446756b25f4
raviveer792 <40062760+raviveer792@users.noreply.github.com>
2018-06-12 07:36:57 -0400

    Add files via upload
    https://github.com/raviveer792/HPE

---
+++ elif.py
@@ -0,0 +1,28 @@
+'''def drive(car_speed):
+	if car_speed>200:
+		print('You r insane')
+	elif car_speed>100:
+		print('TOO FAST')
+	elif car_speed>70 and car_speed<80:
+		print('The optimal speed for ur car')
+	else:
+		print('You r Drirving below the speed')	
+drive(50)
+drive(75)
+drive(150)
+drive(300)
+'''
+def compar(a):
+	if a>1:
+		print('Big')
+	elif a>5:
+		print('Really Big')
+	elif a>10:
+		print('Approaching enormity')
+	elif a>10:
+		print('Enormous!')
+
+compar(0)
+compar(6)
+compar(10)
+compar(11)
