d8fdce4658984d69bae9082fc69d59deada8565d
raviveer792 <40062760+raviveer792@users.noreply.github.com>
2018-07-02 17:47:33 +0530

    Update IMG_Pro.py

--- IMG_Pro.py
+++ IMG_Pro.py
@@ -1,4 +1,5 @@
-from PIL import Image
+# Image data Printing
+'''from PIL import Image
 
 
 pic= Image.open("senna.jpg")
@@ -9,3 +10,19 @@ print(data)
 file=open("data.txt","w")
 for f in data:
     file.write(str(f)+"\n")
+'''
+
+# Image matplot
+from  PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
+pic= Image.open("senna.jpg")
+#pic.show()
+data=np.array(pic)
+#print(data)
+fig=plt.figure()
+ax1=plt.subplot2grid((8,6),(0,0),rowspan=4,colspan=4)
+ax1.imshow(pic)
+plt.show()
+
